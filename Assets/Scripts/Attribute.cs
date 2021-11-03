7d5ba26ded3b18e8ce0faf4245eba2a3c86df947
MichalKrzyzanowski <c00240696@itcarlow.ie>
2021-11-03 20:35:12 +0000

    added attribute script & character template script and prefab

---
+++ Assets/Scripts/Attribute.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Attribute
+{
+    [SerializeField]
+    private string m_name;
+
+    [SerializeField]
+    private float m_value;
+
+    public string name() { return m_name; }
+    public float value() { return m_value; }
+}
---
+++ Assets/Scripts/CharacterTemplate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterTemplate : MonoBehaviour
+{
+    [SerializeField]
+    private string m_name;
+
+    [SerializeField]
+    private bool m_playable;
+
+    [SerializeField]
+    private List<Attribute> m_attributes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
