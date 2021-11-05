092c61ffbd4c046c741dcd82da5355c0263cd89e
C00240696 <C00240696@itcarlow.ie>
2021-11-05 11:35:01 +0000

    added test enviroment

---
+++ Assets/Tests/CharacterTemplateTests.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class TestSuite
+{
+
+    [SetUp]
+    public void Setup()
+    {
+       
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+       
+    }
+
+    // 1
+    [UnityTest]
+    public IEnumerator SetupCombatValues()
+    {
+        yield return null;
+    }
+}
