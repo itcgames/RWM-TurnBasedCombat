f503f87bc36e9eb2a6967ea142c02df6199128fb
MichalKrzyzanowski <c00240696@itcarlow.ie>
2021-11-06 20:51:31 +0000

    finished the code implementation and testing for feature: character template

--- Assets/Scripts/CharacterAttributes.cs
+++ Assets/Scripts/CharacterAttributes.cs
@@ -10,6 +10,12 @@ public class CharacterAttributes : MonoBehaviour
     [SerializeField]
     private bool m_playable;
 
+    public bool Playable
+    {
+        get { return m_playable; }
+        set { m_playable = value; }
+    }
+
     [SerializeField]
     private List<Attribute> m_attributes;
 
@@ -32,6 +38,11 @@ public class CharacterAttributes : MonoBehaviour
         {
             m_attributes[found].Value = value;
         }
+    }
+
+    public void AddAttribute(Attribute attribute)
+    {
+        m_attributes.Add(attribute);
     }
 
     public Attribute FindAttribute(string name)
--- Assets/Tests/CharacterTemplateTests.cs
+++ Assets/Tests/CharacterTemplateTests.cs
@@ -9,7 +9,7 @@ public class TestSuite
     [SetUp]
     public void Setup()
     {
-        m_characterPrefab = Resources.Load("Prefabs/CharacterTemplate") as GameObject;
+        m_characterPrefab = Resources.Load<GameObject>("Prefabs/CharacterTemplate");
     }
 
     [TearDown]
@@ -19,12 +19,42 @@ public class TestSuite
     }
 
     [UnityTest]
-    public IEnumerator SetupCombatValues()
+    public IEnumerator SetupCombatValuesTest()
     {
         m_characterPrefab = Object.Instantiate(m_characterPrefab);
         m_characterPrefab.GetComponent<CharacterAttributes>().SetAttribute("Hit Points", 200);
         yield return new WaitForSeconds(0.1f);
 
         Assert.AreEqual(m_characterPrefab.GetComponent<CharacterAttributes>().FindAttribute("Hit Points").Value, 200);
+    }
+
+    [UnityTest]
+    public IEnumerator SetupSpriteTest()
+    {
+        m_characterPrefab = Object.Instantiate(m_characterPrefab);
+        m_characterPrefab.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("Sprite/ship");
+        yield return new WaitForSeconds(0.1f);
+
+        Assert.AreEqual(m_characterPrefab.GetComponent<SpriteRenderer>().sprite.name, "ship");
+    }
+
+    [UnityTest]
+    public IEnumerator playableStatusTest()
+    {
+        m_characterPrefab = Object.Instantiate(m_characterPrefab);
+        m_characterPrefab.GetComponent<CharacterAttributes>().Playable = true;
+        yield return new WaitForSeconds(0.1f);
+
+        Assert.AreEqual(m_characterPrefab.GetComponent<CharacterAttributes>().Playable, true);
+    }
+
+    [UnityTest]
+    public IEnumerator customAttributeTest()
+    {
+        m_characterPrefab = Object.Instantiate(m_characterPrefab);
+        m_characterPrefab.GetComponent<CharacterAttributes>().AddAttribute(new Attribute("Luck", 20));
+        yield return new WaitForSeconds(0.1f);
+
+        Assert.AreEqual(m_characterPrefab.GetComponent<CharacterAttributes>().FindAttribute("Luck").Name, "Luck");
     }
 }
