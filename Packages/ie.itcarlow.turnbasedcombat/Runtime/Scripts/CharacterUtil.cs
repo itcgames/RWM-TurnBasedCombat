54d55390b4e898ae4dd3a6e5d768a74e56d7a3eb
Michał Krzyżanowski <45101664+MichalKrzyzanowski@users.noreply.github.com>
2022-01-22 14:02:24 +0000

    added turn order implementation and test (#2)

---
+++ Packages/ie.itcarlow.turnbasedcombat/Runtime/Scripts/CharacterUtil.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterUtil
+{
+  public static GameObject CreateCharacter(CharacterAttributes attrs, string spritePath)
+    {
+        GameObject character = new GameObject();
+        character.AddComponent<CharacterAttributes>();
+        character.GetComponent<CharacterAttributes>().Name = attrs.Name;
+        character.GetComponent<CharacterAttributes>().Playable = attrs.Playable;
+        character.GetComponent<CharacterAttributes>().Attributes = attrs.Attributes;
+
+        if(spritePath != null || spritePath != "")
+        {
+            character.AddComponent<SpriteRenderer>();
+            character.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(spritePath);
+        }
+
+        Debug.Log(character.GetComponent<CharacterAttributes>().Name);
+
+        return character;
+    }
+}
---
+++ Packages/ie.itcarlow.turnbasedcombat/Tests/Runtime/TurnOrderTests.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Collections;
+
+public class TurnOrderTests
+{
+    private GameObject m_player;
+    
+    [SetUp]
+    public void Setup()
+    {
+        CharacterAttributes attrs = new CharacterAttributes();
+        attrs.Name = "Frank";
+        m_player = CharacterUtil.CreateCharacter(attrs, null);
+    }
+
+    [TearDown]
+    public void Teardown()
+    {
+        Object.Destroy(m_player);
+    }
+
+    [UnityTest]
+    public IEnumerator TurnOrderTest()
+    {
+        GameObject gameController = new GameObject();
+        gameController.AddComponent<TurnOrder>();
+        gameController.AddComponent<FirstStrikeChance>();
+        gameController.AddComponent<EnemyGen>();
+
+        gameController.GetComponent<FirstStrikeChance>().SetType(FirstStrikeChance.CheckType.BooleanExpressions);
+        gameController.GetComponent<FirstStrikeChance>().SetOnAdvantage(false);
+
+        gameController.GetComponent<EnemyGen>().GenerateEnemies(1);
+
+        List<GameObject> playables = new List<GameObject>();
+        playables.Add(m_player);
+
+        Dictionary<int, GameObject> expectedOrder = gameController.GetComponent<TurnOrder>().DecideTurnOrder(playables, gameController.GetComponent<EnemyGen>().EnemyList);
+
+        yield return new WaitForSeconds(0.1f);
+
+        Assert.AreEqual(expectedOrder[2].GetComponent<CharacterAttributes>().Name, "Frank");
+    }
+}
